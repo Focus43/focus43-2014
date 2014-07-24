de2b937c92b52a5671f7cb2c6d03178e9808a905
Jonathan Hartman <jhartman86@gmail.com>
2014-07-24 13:34:07 -0600

    Parallax stuff and canvas coming

--- web/packages/focus43/themes/f43/default.php
+++ web/packages/focus43/themes/f43/default.php
@@ -5,7 +5,7 @@
 <?php Loader::element('header_required'); // REQUIRED BY C5 // ?>
 </head>
 
-<body scroll-handler=".layer" data-scroll-target="#content">
+<body animate>
 
     <div id="parallax">
         <div class="inner">
@@ -16,33 +16,85 @@
     </div>
 
     <div id="content">
-        <section id="section-home">
-            <div class="tabular">
-                <div class="cellular">
-                    <div id="stacked" animations>
-                        <h1>Focus43</h1>
-                        <span>One</span><span>Two</span>
+        <div id="track">
+            <section id="section-home">
+                <div class="tabular">
+                    <div class="cellular" style="position:relative;">
+                        <h1>
+                            <span>w</span>
+                            <span>e</span>
+                            <span>&nbsp;</span>
+                            <span>c</span>
+                            <span>r</span>
+                            <span>a</span>
+                            <span>f</span>
+                            <span>t</span>
+                            <span>&nbsp;</span>
+                            <span>e</span>
+                            <span>x</span>
+                            <span>p</span>
+                            <span>e</span>
+                            <span>r</span>
+                            <span>i</span>
+                            <span>e</span>
+                            <span>n</span>
+                            <span>c</span>
+                            <span>e</span>
+                            <span>s</span>
+                        </h1>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
 
-        <section id="section-about">
-            <p>About</p>
-        </section>
+            <section id="section-about">
+                <div class="subscroller">
+                    <div class="inside-track">
+                        <div class="pane">
+                            <img src="<?php echo FOCUS43_IMAGE_PATH; ?>screenshot.png" alt="" />
+                        </div>
+                        <div class="pane">
+                            <img src="<?php echo FOCUS43_IMAGE_PATH; ?>screenshot.png" alt="" />
+                        </div>
+                        <div class="pane">
+                            <img src="<?php echo FOCUS43_IMAGE_PATH; ?>screenshot.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div class="descriptions">
+                    <div class="node tabular">
+                        <div class="cellular">
+                            <h2>Town Of Jackson (Municipality)</h2>
+                            <p>Lorem ipsum dolor sit amet consect et tetur</p>
+                        </div>
+                    </div>
+                    <div class="node tabular">
+                        <div class="cellular">
+                            <h2>Another Project</h2>
+                            <p>Frack yea dude</p>
+                        </div>
+                    </div>
+                    <div class="node tabular">
+                        <div class="cellular">
+                            <h2>A third project yea?</h2>
+                            <p>So fly baby so fly</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-        <section id="section-work">
-            <p>Our work</p>
-        </section>
+            <section id="section-work">
+                <div class="rotated"></div>
+            </section>
 
-        <section id="section-experiments">
-            <p>Experiments</p>
-            <a href="/experiments/this-is-the-experiment-name">Click here</a>
-        </section>
+            <section id="section-experiments">
+                <p>Experiments</p>
+                <a href="/experiments/this-is-the-experiment-name">Click here</a>
+            </section>
 
-        <section id="section-contact">
-            <p>Contact Us</p>
-        </section>
+            <section id="section-contact">
+                <p>Contact Us</p>
+            </section>
+        </div>
     </div>
 
     <div ng-view></div>
