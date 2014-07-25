f686122d8ff5d37622cc30a13a4809190e11ef7d
Jonathan Hartman <jhartman86@gmail.com>
2014-07-25 02:00:19 -0600

    Working with hammerjs on scroll adjustments

--- web/packages/focus43/js/app-src/common/directives/animate.js
+++ web/packages/focus43/js/app-src/common/directives/animate.js
@@ -1,20 +1,27 @@
 /* global requestAnimationFrame */
+/* global ThrowPropsPlugin */
+/* global Hammer */
 
 angular.module('f43.common')
 
     .directive('animate', ['$window', 'TweenLite', 'TimelineLite',
         function( $window, TweenLite, TimelineLite ){
 
-            var elTrack         = document.querySelector('#track'),
+            var elContent       = document.querySelector('#content'),
+                elFalseTrack    = document.querySelector('#falseTrack'),
+                elTrack         = document.querySelector('#track'),
                 parallaxLayers  = document.querySelectorAll('#parallax .layer'),
                 sections        = document.querySelectorAll('section'),
-                timelineMaster  = new TimelineLite({paused:true}),
+                timelineMaster  = new TimelineLite({
+                    paused:true
+                    //smoothChildTiming:true
+                }),
                 winH, winW, trackH, trackW;
 
             function processDimensions(){
                 winH = document.documentElement.clientHeight;
                 winW = document.body.clientWidth;
-                trackH = elTrack.clientHeight;
+                trackH = elFalseTrack.clientHeight;
                 trackW = winW;
             }
 
@@ -22,70 +29,80 @@ angular.module('f43.common')
                 return Math.floor(Math.random() * (max - min + 1)) + min;
             }
 
+            function tlInstance(_params){
+                return new TimelineLite(_params);
+            }
+
 
             /**
              * @returns TimelineLite
              */
             function timelineSection1(){
-                var _timeline = (new TimelineLite()).
-                    to(elTrack, 0, {y:0, ease:Power2.easeIn, roundProps:"y"}, 0);
-                    //staggerTo(sections[0].querySelectorAll('h1 span'), 0.15, {scale:random(2,3), opacity:0}, 0.05);
-
-                return _timeline;
+                return tlInstance()
+                    .to(elTrack, 0, {y:0});
             }
 
             /**
              * @returns TimelineLite
              */
             function timelineSection2(){
-                TweenLite.set(sections[1].querySelector('.inside-track'), {
-                    transformOrigin:'50% 100%'
-                });
+                return tlInstance()
+                    .to(elTrack, 1, {y:-(winH)});
 
-                var _timeline = (new TimelineLite()).
-                    to(elTrack, 1, {y:-(winH), ease:Power2.easeIn, roundProps:"y"}, 0).
-                    fromTo(sections[1].querySelector('.pane'), 1, {top:'100%'}, {top:0}).
-                    to(sections[1].querySelector('.node:nth-child(1)'), 0.5, {opacity:1, backgroundColor:'rgba(255,255,255,0.5)'}).
-                    to(sections[1].querySelector('.inside-track'), 2, {left:'-=100%', delay:1}).
-                    to(sections[1].querySelector('.node:nth-child(1)'), 0.5, {opacity:0}).
-                    to(sections[1].querySelector('.node:nth-child(2)'), 0.5, {opacity:1, backgroundColor:'rgba(15,15,79,0.8)'}, '-=0.5').
-                    to(sections[1].querySelector('.inside-track'), 2, {left:'-=100%'}).
-                    to(sections[1], 1, {rotationX:20, rotationY:20, scale:0.7}).
-                    to(sections[1].querySelector('.subscroller'), 1, {z: 10}).
-                    to(sections[1].querySelector('.inside-track'), 1, {z: 50, rotationX: 90});
-
-
-                return _timeline;
+//                TweenLite.set(sections[1].querySelector('.inside-track'), {
+//                    transformOrigin:'50% 100%'
+//                });
+//
+//                var _timeline = (new TimelineLite()).
+//                    to(elTrack, 1, {y:-(winH), ease:Power2.easeIn, roundProps:"y"}, 0).
+//                    fromTo(sections[1].querySelector('.pane'), 1, {top:'100%'}, {top:0}).
+//                    to(sections[1].querySelector('.node:nth-child(1)'), 0.5, {opacity:1, backgroundColor:'rgba(255,255,255,0.5)'}).
+//                    to(sections[1].querySelector('.inside-track'), 2, {left:'-=100%', delay:1}).
+//                    to(sections[1].querySelector('.node:nth-child(1)'), 0.5, {opacity:0}).
+//                    to(sections[1].querySelector('.node:nth-child(2)'), 0.5, {opacity:1, backgroundColor:'rgba(15,15,79,0.8)'}, '-=0.5').
+//                    to(sections[1].querySelector('.inside-track'), 2, {left:'-=100%'}).
+//                    to(sections[1], 1, {rotationX:20, rotationY:20, scale:0.7}).
+//                    to(sections[1].querySelector('.subscroller'), 1, {z: 10}).
+//                    to(sections[1].querySelector('.inside-track'), 1, {z: 50, rotationX: 90});
+//
+//
+//                return _timeline;
             }
 
             function timelineSection3(){
-                var _timeline = (new TimelineLite()).
-                    to(elTrack, 1, {y:-(winH*2), ease:Power2.easeIn, roundProps:"y"}, 0).
-                    fromTo(sections[2].querySelector('.rotated'), 2, {left:'-100%'}, {left:0, ease:Power2.easeInOut});
-
-                return _timeline;
+                return tlInstance()
+                    .to(elTrack, 1, {y:-(winH*2)});
+//                var _timeline = (new TimelineLite()).
+//                    to(elTrack, 1, {y:-(winH*2), ease:Power2.easeIn, roundProps:"y"}, 0).
+//                    fromTo(sections[2].querySelector('.rotated'), 2, {left:'-100%'}, {left:0, ease:Power2.easeInOut});
+//
+//                return _timeline;
             }
 
             function timelineSection4(){
-                var _timeline = (new TimelineLite())
-                    .to(elTrack, 1, {y:-(winH*3), ease:Power2.easeIn, roundProps:"y"}, 0);
-                return _timeline;
+                return tlInstance()
+                    .to(elTrack, 1, {y:-(winH*3)});
+//                var _timeline = (new TimelineLite())
+//                    .to(elTrack, 1, {y:-(winH*3), ease:Power2.easeIn, roundProps:"y"}, 0);
+//                return _timeline;
             }
 
             function timelineSection5(){
-                var _timeline = (new TimelineLite())
-                    .to(elTrack, 1, {y:-(winH*4), ease:Power2.easeIn, roundProps:"y"}, 0);
-                return _timeline;
+                return tlInstance();
+//                var _timeline = (new TimelineLite())
+//                    .to(elTrack, 1, {y:-(winH*4), ease:Power2.easeIn, roundProps:"y"}, 0);
+//                return _timeline;
             }
 
             function buildTimelines(){
                 // Nest the timelines into the master
                 timelineMaster
                     .add(timelineSection1(), 'section1')
-                    .add(timelineSection2(), 'section2')
-                    .add(timelineSection3(), 'section3')
-                    .add(timelineSection4(), 'section4')
-                    .add(timelineSection5(), 'section5');
+                    .add(timelineSection2(), 'section2')//.addPause()
+                    .add(timelineSection3(), 'section3')//.addPause()
+                    .add(timelineSection4(), 'section4');//.addPause();
+//                    .add(timelineSection4(), 'section4')
+//                    .add(timelineSection5(), 'section5');
 
                 $window['tlMaster'] = timelineMaster;
             }
@@ -94,11 +111,82 @@ angular.module('f43.common')
                 processDimensions();
                 buildTimelines();
 
-                (function draw(){
-                    Controller.progressTo(window.scrollY);
-                    requestAnimationFrame(draw);
-                })();
+                var _hammer = new Hammer(document.documentElement);
+                _hammer.get('pan').set({direction: Hammer.DIRECTION_VERTICAL});
 
+
+                var lastMove = 0,
+                    trackY   = 0,
+                    duration = timelineMaster.totalDuration(),
+                    notch    = duration / trackH;
+
+                ThrowPropsPlugin.track(elTrack, 'y');
+
+                _hammer.on('pan', function(event){
+                    console.log(event);
+//                    if( event.timeStamp - lastMove > 200 ){
+//                        console.log(event.deltaY);
+//                        lastMove = event.timeStamp;
+//                        trackY = trackY + (event.deltaY * (notch*100));
+//                        TweenLite.to(elTrack, 0.25, {
+//                            y: trackY,
+//                            ease: Power2.easeOut
+//                        });
+//                    }
+
+                    //ThrowPropsPlugin.track(elTrack, 'y');
+//                    TweenLite.to(elTrack, 0.1, {
+//                        y: trackY,
+//                        ease: Power2.easeOut
+//                    });
+
+//                    if( (event.timeStamp - lastMove) > 200 ){
+//                        lastMove = event.timeStamp;
+//                        trackY   = trackY - (event.velocityY * 100);
+//                        TweenLite.to(elTrack, 0.5, {y: trackY, ease:Power2.easeOut});
+//
+//                        //console.log(event.velocityY, timelineMaster.totalTime() + (timelineMaster.totalTime() * event.velocityY));
+//
+////                        TweenLite.to(timelineMaster, 0.2, {
+////                            time: event.velocityY * duration //((event.velocityY * 5)/100) + timelineMaster.progress()
+////                        });
+//
+////                        console.log('playTo', (notch / (1 - event.velocityY))*duration );
+////                        TweenLite.to(timelineMaster, 0.2, {
+////                            progress: timelineMaster.progress() + (1-event.velocityY)
+////                        });
+//
+////                        TweenLite.to(timelineMaster, 0.2,
+////                            {progress: timelineMaster.progress() + event.velocityY}
+////                            //{time: timelineMaster.totalTime() + (timelineMaster.totalTime() * event.velocityY)}
+////                            //{time: (window.scrollY / (trackH - winH)) * timelineMaster.totalDuration()}
+////                            //{ThrowProps: {time: timelineMaster.totalDuration() + _to}}
+////                        );
+//                        //timelineMaster.totalTime(_scroll * timelineMaster.totalDuration());
+//                    }
+                });
+
+
+
+                document.addEventListener('touchmove', function(e){
+                    e.preventDefault();
+                }, false);
+
+//                (function _draw(){
+//                    //console.log(window.scrollY, 'ON ANIMATION FRAME');
+//                    //Controller.progressTo(window.scrollY);
+//                    requestAnimationFrame(_draw);
+//                })();
+
+//                $window.addEventListener('touchmove', function(ev){
+//                    event.preventDefault();
+//                    console.log(ev);
+//                }, false);
+
+//                Draggable.create(document.body, {
+//                    type: 'scrollTop',
+//                    edgeResistance: 0.5
+//                });
 
 //                Draggable.create(elTrack, {
 //                    type:'scrollTop',
@@ -110,14 +198,27 @@ angular.module('f43.common')
 //                });
 
 
+                //ThrowPropsPlugin.track(timelineMaster, 'time');
+
 //                angular.element($window)
-//                    // "onWindowScroll"
-//                    .on('scroll', function(){
-//                        // Set the background parallax position independently
-//                        Controller.progressTo(this.pageYOffset);
+//                    .on('scroll', function(ev){
+//                        console.log('scroll event triggered!', window.scrollY);
+//                        //Controller.progressTo(window.scrollY);
 //                    })
-//                    // "onWindowResize"
-//                    .on('resize', processDimensions);
+//                    .on('mousewheel', function(ev){
+//                        //console.log(ev.deltaY);
+//                        //Controller.progressTo( (window.scrollY) )
+//                        //console.log( (window.scrollY) );
+//                        //var _to = timelineMaster.totalDuration() * (ev.deltaY/1000);
+//
+////                        TweenLite.to(timelineMaster, 0.25,
+////                            {time: (window.scrollY / (trackH - winH)) * timelineMaster.totalDuration()}
+////                            //{ThrowProps: {time: timelineMaster.totalDuration() + _to}}
+////                        );
+//                        //timelineMaster.totalTime(_scroll * timelineMaster.totalDuration());
+//
+//                        //console.log(ev);
+//                    });
             }
 
             return {
@@ -136,9 +237,16 @@ angular.module('f43.common')
                     self.progressTo = function( verticalOffset ){
                         var _scroll = (verticalOffset / (trackH - winH)),
                             _moveX  = _scroll * winW;
-                        //console.log(verticalOffset);
                         //TweenLite.set(parallaxLayers, {x:-_moveX});
-                        timelineMaster.progress(_scroll);
+                        //timelineMaster.progress(_scroll);
+
+//                        TweenLite.to(timelineMaster, 0.75,
+//                            //{time: _scroll * timelineMaster.totalDuration()}
+//                            {ThrowProps: {time: _scroll * timelineMaster.totalDuration()}}
+//                        );
+
+//                        ThrowPropsPlugin.track(timelineMaster, 'time');
+//                        timelineMaster.totalTime(_scroll * timelineMaster.totalDuration());
                     };
 
                 }]
--- web/packages/focus43/themes/f43/default.php
+++ web/packages/focus43/themes/f43/default.php
@@ -7,6 +7,8 @@
 
 <body animate>
 
+    <div id="falseTrack"><!-- just to control the height! --></div>
+
     <div id="parallax">
         <div class="inner">
             <div id="layer-butte" class="layer"></div>
@@ -21,78 +23,44 @@
                 <div class="tabular">
                     <div class="cellular" style="position:relative;">
                         <h1>
-                            <span>w</span>
-                            <span>e</span>
-                            <span>&nbsp;</span>
-                            <span>c</span>
-                            <span>r</span>
-                            <span>a</span>
-                            <span>f</span>
-                            <span>t</span>
-                            <span>&nbsp;</span>
-                            <span>e</span>
-                            <span>x</span>
-                            <span>p</span>
-                            <span>e</span>
-                            <span>r</span>
-                            <span>i</span>
-                            <span>e</span>
-                            <span>n</span>
-                            <span>c</span>
-                            <span>e</span>
-                            <span>s</span>
+                            focus43
                         </h1>
                     </div>
                 </div>
             </section>
 
             <section id="section-about">
-                <div class="subscroller">
-                    <div class="inside-track">
-                        <div class="pane">
-                            <img src="<?php echo FOCUS43_IMAGE_PATH; ?>screenshot.png" alt="" />
-                        </div>
-                        <div class="pane">
-                            <img src="<?php echo FOCUS43_IMAGE_PATH; ?>screenshot.png" alt="" />
-                        </div>
-                        <div class="pane">
-                            <img src="<?php echo FOCUS43_IMAGE_PATH; ?>screenshot.png" alt="" />
-                        </div>
-                    </div>
-                </div>
-                <div class="descriptions">
-                    <div class="node tabular">
-                        <div class="cellular">
-                            <h2>Town Of Jackson (Municipality)</h2>
-                            <p>Lorem ipsum dolor sit amet consect et tetur</p>
-                        </div>
-                    </div>
-                    <div class="node tabular">
-                        <div class="cellular">
-                            <h2>Another Project</h2>
-                            <p>Frack yea dude</p>
-                        </div>
-                    </div>
-                    <div class="node tabular">
-                        <div class="cellular">
-                            <h2>A third project yea?</h2>
-                            <p>So fly baby so fly</p>
-                        </div>
+                <div class="tabular">
+                    <div class="cellular">
+                        <h2>Focus43 Oh Yea</h2>
+                        <img src="<?php echo FOCUS43_IMAGE_PATH; ?>screenshot.png" class="img-responsive" />
                     </div>
                 </div>
             </section>
 
             <section id="section-work">
-                <div class="rotated"></div>
+                <div class="tabular">
+                    <div class="cellular">
+                        this be the work section
+                    </div>
+                </div>
             </section>
 
             <section id="section-experiments">
-                <p>Experiments</p>
-                <a href="/experiments/this-is-the-experiment-name">Click here</a>
+                <div class="tabular">
+                    <div class="cellular">
+                        <p>Experiments</p>
+                        <p><a href="/experiments/this-is-the-experiment-name">Click here</a></p>
+                    </div>
+                </div>
             </section>
 
             <section id="section-contact">
-                <p>Contact Us</p>
+                <div class="tabular">
+                    <div class="cellular">
+                        <p>COntact Us</p>
+                    </div>
+                </div>
             </section>
         </div>
     </div>
