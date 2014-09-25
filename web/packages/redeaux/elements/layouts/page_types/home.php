7c7e7782956500038cc1dad1c79198636072cae9
Jonathan Hartman <jhartman86@gmail.com>
2014-09-25 14:07:01 -0600

    Moving on

--- web/packages/redeaux/elements/layouts/page_types/home.php
+++ web/packages/redeaux/elements/layouts/page_types/home.php
@@ -6,35 +6,33 @@
     </div>
 
     <div class="page-content">
-        <div class="slanter">
-            <div class="reverse">
-                <div class="tabular">
-                    <div class="cellular">
-                        <h1>Hi. We're<br />Focus43.<span>We craft web &amp; iOS experiences.</span></h1>
-                    </div>
+        <div class="tabular">
+            <div class="cellular">
+                <div class="grouper">
+                    <span>Hi, we're</span>
+                    <span>Focus43</span>
+                    <span>web &amp; iOS <i class="fa fa-code"></i>s</span>
                 </div>
             </div>
         </div>
+    </div>
 
-        <!--<div class="tabular">
+    <div class="page-content" style="">
+        <svg class="slant" version="1.1" width="200" height="200" x="0" y="0" viewBox="0 0 200 200" preserveAspectRatio="none">
+            <!--<path stroke="none" d="M0 0 L 200 0 L 125 200 L 0 200 Z" />-->
+            <path stroke="none" d="M100 0 L200 0 L100 200 L0 200 Z" />
+            <path stroke="none" d="M0 0 L100 0 L200 200 L100 200 Z" />
+        </svg>
+        <div class="tabular">
             <div class="cellular">
-                <div class="inner">
-                    <img src="<?php echo REDEAUX_IMAGE_PATH; ?>logo.png" />
-                    <div class="slanter">
-                        <h1>We're Focus43</h1>
-                        <?php //$a = new Area('Default'); $a->display($c); ?>
-                    </div>
-                    <div class="slanter2">
-                        <h2>We build engaging web &amp; iOS stuff</h2>
-                    </div>
-                    <div class="slanter3">
-                        <a class="q">About Us</a>
-                        <a class="q">Work</a>
-                        <a class="q">Experiments</a>
-                        <a class="q">Contact</a>
-                    </div>
+                <div class="intro">
+                    <svg class="agon" version="1.1" width="100" height="100" viewBox="0 0 100 100">
+                        <polygon points="50,0 95,25 95,75, 50,100 5,75 5,25"></polygon>
+                    </svg>
+                    <!--<h1>Hi. We're<br />Focus43.<span>We craft web &amp; iOS experiences.</span></h1>-->
+                    <i class="fa fa-code"></i>
                 </div>
             </div>
-        </div>-->
+        </div>
     </div>
 </div>
