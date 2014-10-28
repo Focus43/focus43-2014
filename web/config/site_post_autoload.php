ba74e256074cdc58d8348319e04ae138b781b238
Jonathan Hartman <jhartman86@gmail.com>
2014-10-28 16:27:41 -0600

    Commenting out stuff

--- web/config/site_post_autoload.php
+++ web/config/site_post_autoload.php
@@ -1,8 +1,8 @@
 <?php defined('C5_EXECUTE') or die(_("Access Denied."));
 
     // Custom for Focus43 site; override the core request library
-    $_envObj = Environment::get();
-    $_envObj->overrideCoreByPackage('libraries/request.php', 'focus43');
+    //$_envObj = Environment::get();
+    //$_envObj->overrideCoreByPackage('libraries/request.php', 'focus43');
 
 	// if the Redis connection handle is defined, we're using Redis for something
 	if( defined('REDIS_CONNECTION_HANDLE') ){
