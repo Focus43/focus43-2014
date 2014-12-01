dcb37b66a612087f0ba4fdf38921225428ee9519
Jonathan Hartman <jhartman86@gmail.com>
2014-12-01 12:39:41 -0700

    Updated gulpfile.js in root

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,9 @@
     // Path to packages directory
     var _packages = __dirname + '/web/packages/';
 
-    // Search all packages for gulp.js at the root level
-    glob("**/gulp.js", {cwd: _packages, sync:true}, function(err, files){
-        files.forEach(function(filename){
-            require(_packages + filename)(gulp);
-        });
+    // Search all packages for gulp.js file
+    glob.sync('**/gulp.js', {cwd: __dirname + '/web/packages/'}).forEach(function(fileName){
+        require(_packages + fileName)(gulp);
     });
 
     // Register 'watch' as the default task
